41230799c1f8b25b80f7b8bd603891db219b035d
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2022-02-16 20:05:05 -0300

    Initial commit

---
+++ src/main/java/com/ongames/demo/LocadoraApplication.java
@@ -0,0 +1,13 @@
+package com.ongames.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraApplication.class, args);
+	}
+
+}

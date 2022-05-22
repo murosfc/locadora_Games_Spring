dd2b10df2c922165b0d7f2118720015c63546ed0
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2022-05-22 18:29:52 -0300

    Estudos de segurança e autorização de acesso

--- src/main/java/com/ongames/security/LocadoraWebSecurity.java
+++ src/main/java/com/ongames/security/LocadoraWebSecurity.java
@@ -1,6 +1,7 @@
 package com.ongames.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,12 +23,21 @@ public class LocadoraWebSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/apirest/**")
                 .and()
-                .authorizeRequests()                
+                .authorizeRequests() 
+                    .antMatchers(HttpMethod.DELETE).hasAnyRole("ADMIN", "FUNC")
+                    .antMatchers(HttpMethod.POST).hasAnyRole("ADMIN", "FUNC")
+                    .antMatchers(HttpMethod.PUT).hasAnyRole("ADMIN", "FUNC")
+                    .antMatchers(HttpMethod.PATCH).hasAnyRole("ADMIN", "FUNC")
                     .antMatchers("/apirest/**").hasRole("ADMIN")
-                    .antMatchers("/funcionarios/**").hasRole("ADMIN")
+                    .antMatchers("/funcionarios/**").hasRole("ADMIN")                    
+                    .antMatchers("/pagamentos/**").hasRole("ADMIN")
+                    .antMatchers("/jogos/**").hasAnyRole("ADMIN", "FUNC")
+                    .antMatchers("/clientes/**").hasAnyRole("ADMIN", "FUNC")
+                    .antMatchers("/categorias/**").hasAnyRole("ADMIN", "FUNC")
+                    .antMatchers("/contas/**").hasAnyRole("ADMIN", "FUNC")
+                    .antMatchers("/alugueis/**").hasAnyRole("ADMIN", "FUNC") 
                     .antMatchers("/funcionarios/meusDados/**").hasAnyRole("ADMIN", "FUNC")
-                    .antMatchers("/pagamentos/**").hasRole("ADMIN")                
-                    .anyRequest().authenticated()               
+                    .anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .and()
